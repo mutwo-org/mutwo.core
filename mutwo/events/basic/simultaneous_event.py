36254b55b6cefcf8722d879eae388c071ac31e25
levinericzimmermann <levin.eric.zimmermann@posteo.eu>
2020-12-05 01:34:56 +0100

    improved doc string of SimultaneousEvent

--- mutwo/events/basic/simultaneous_event.py
+++ mutwo/events/basic/simultaneous_event.py
@@ -2,7 +2,7 @@ from mutwo import events
 
 
 class SimultaneousEvent(events.abc.ComplexEvent):
-    """Event-Object, which contains other Event-Objects, which happen simultaneously."""
+    """Event-Object, which contains other Event-Objects, which happen at the same time."""
 
     @events.abc.ComplexEvent.duration.getter
     def duration(self):
