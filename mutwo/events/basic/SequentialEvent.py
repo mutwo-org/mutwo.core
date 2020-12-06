7286a10f8fbe1bdabbef1638b36ea375e53ed25f
levinericzimmermann <levin.eric.zimmermann@posteo.eu>
2020-12-06 22:55:23 +0100

    added exact typing to absolute_points; made docstring fit 88 character limit

--- mutwo/events/basic/SequentialEvent.py
+++ mutwo/events/basic/SequentialEvent.py
@@ -1,20 +1,19 @@
 import itertools
 import numbers
-import typing
 
 from mutwo import events
 from mutwo.utilities import tools
 
 
 class SequentialEvent(events.abc.ComplexEvent):
-    """Event-Object which contains other Event-Objects which happen in a linear order."""
+    """Event-Object which contains other Events which happen in a linear order."""
 
     @events.abc.ComplexEvent.duration.getter
     def duration(self):
         return sum(event.duration for event in self)
 
     @property
-    def absolute_times(self) -> typing.Sequence:
+    def absolute_times(self) -> tuple:
         """Return absolute point in time for each event."""
         durations = (event.duration for event in self)
         return tuple(tools.accumulate_from_zero(durations))[:-1]
