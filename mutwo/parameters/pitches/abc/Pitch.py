390b80bc7439644e1d0b80820ae6fdaa12346bbd
levinericzimmermann <levin.eric.zimmermann@posteo.eu>
2020-12-07 21:51:08 +0100

    added abstract property cents to Pitch class

--- mutwo/parameters/pitches/abc/Pitch.py
+++ mutwo/parameters/pitches/abc/Pitch.py
@@ -5,9 +5,16 @@ from mutwo import parameters
 
 
 class Pitch(parameters.abc.Parameter):
+    """Abstract blueprint for any pitch class."""
+
     @property
     @abc.abstractmethod
     def frequency(self) -> float:
+        raise NotImplementedError
+
+    @property
+    @abc.abstractmethod
+    def cents(self) -> float:
         raise NotImplementedError
 
     @property
