3134a317a90b81b84377012fdbca2672766271f0
levinericzimmermann <levin.eric.zimmermann@posteo.eu>
2020-12-16 10:18:29 +0100

    added explicit tests for prime_factors

---
+++ tests/utilities/prime_factors_test.py
@@ -0,0 +1,43 @@
+import functools
+import operator
+import unittest
+
+from mutwo.utilities import prime_factors
+
+
+class PrimeFactorsTest(unittest.TestCase):
+    def test_is_prime(self):
+        self.assertEqual(prime_factors.is_prime(4), False)
+        self.assertEqual(prime_factors.is_prime(3), True)
+        self.assertEqual(prime_factors.is_prime(100), False)
+        self.assertEqual(prime_factors.is_prime(23), True)
+        self.assertEqual(prime_factors.is_prime(19), True)
+        self.assertEqual(prime_factors.is_prime(18), False)
+
+    def test_factorise(self):
+        factorised_numbers = ([2, 2], [2, 3, 13, 23, 31], [3, 5, 7], [11, 11, 11])
+        for factorised_number in factorised_numbers:
+            self.assertEqual(
+                prime_factors.factorise(
+                    functools.reduce(operator.mul, factorised_number)
+                ),
+                factorised_number,
+            )
+
+    def test_factors(self):
+        factors = ([(2, 1), (3, 4), (5, 1), (11, 1)], [(5, 10)])
+        for primes_and_exponents in factors:
+            number = functools.reduce(
+                operator.mul,
+                [
+                    operator.pow(*prime_and_exponent)
+                    for prime_and_exponent in primes_and_exponents
+                ],
+            )
+            self.assertEqual(
+                list(prime_factors.factors(number)), primes_and_exponents,
+            )
+
+
+if __name__ == "__main__":
+    unittest.main()
