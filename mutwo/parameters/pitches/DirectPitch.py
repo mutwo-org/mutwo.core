6b8c050f1eab74d8764ca0188f9a753b20e9b3ed
levinericzimmermann <levin.eric.zimmermann@posteo.eu>
2021-01-02 22:47:42 +0100

    added docstring for DirectPitch

--- mutwo/parameters/pitches/DirectPitch.py
+++ mutwo/parameters/pitches/DirectPitch.py
@@ -2,6 +2,13 @@ from mutwo.parameters import pitches
 
 
 class DirectPitch(pitches.abc.Pitch):
+    """A simple pitch class that gets directly initialised by its frequency.
+
+    May be used when a converter class needs a pitch object, but there is
+    no need or desire for a complex abstraction of the respective pitch
+    (that classes like JustIntonationPitch or WesternPitch offer).
+    """
+
     def __init__(self, frequency: float):
         self._frequency = frequency
 
