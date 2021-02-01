5d02897807d62b20db6d8e65cffe47dd697e5b29
levinericzimmermann <levin.eric.zimmermann@posteo.eu>
2021-02-01 12:36:40 +0100

    added 'generators' module with first submodule 'gray'

---
+++ mutwo/generators/__init__.py
@@ -0,0 +1,11 @@
+"""Module that contains classes and functions that generate data with the potential of artistic use.
+
+The module is organised in different submodules where each submodule is named after the first known
+person who introduced the respective algorithms. Unlike the 'converters' module the entered data and
+the resulting data can be very different in type and form.
+
+The term 'generators' simply labels the functionality of the module and shouldn't be confused with the Python
+term for a specific function type.
+"""
+
+from . import gray
---
+++ mutwo/generators/gray/__init__.py
@@ -0,0 +1,3 @@
+"""Module for algorithms which are related to the US-American researcher Frank Gray."""
+
+from .reflected_binary_code import reflected_binary_code
