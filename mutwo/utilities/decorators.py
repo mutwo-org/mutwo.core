a021a17b96f4323e3fdb9d14b30b88d934897fc0
levinericzimmermann <levin.eric.zimmermann@posteo.eu>
2021-02-03 22:19:58 +0100

    added docstring

--- mutwo/utilities/decorators.py
+++ mutwo/utilities/decorators.py
@@ -1,9 +1,23 @@
+"""Module for generic decorators that are used within mutwo."""
+
 import copy
 import functools
 import typing
 
 
 def add_return_option(function: typing.Callable) -> typing.Callable:
+    """This decorator adds a return option for object mutating methods.
+
+    The 'add_return_option' decorator adds the 'mutate' keyword argument
+    to the decorated method. If 'mutate' is set to False, the decorator deep
+    copies the respective object, then applies the called method on the new
+    copied object and finally returns the copied object. This can be useful
+    for methods that by default mutate its object. When adding this method,
+    it is up to the user whether the original object shall be changed
+    (for mutate=True) or if a copied version of the object with the
+    respective mutation shall be returned (for mutate=False).
+    """
+
     @functools.wraps(function)
     def wrapper(self, *args, mutate: bool = True, **kwargs) -> typing.Any:
         if mutate is True:
