465057fb0c3c197a4a01cfe74c513809dd3576db
levinericzimmermann <levin.eric.zimmermann@posteo.eu>
2021-02-07 13:58:14 +0100

    added format 64bit

--- mutwo/converters/frontends/csound_constants.py
+++ mutwo/converters/frontends/csound_constants.py
@@ -3,6 +3,7 @@
 SILENT_FLAG = '--no-displays'
 FORMAT_IRCAM = '--format=ircam'
 FORMAT_24BIT = '--format=24bit'
+FORMAT_64BIT = '--format=double'
 FORMAT_8BIT = '--format=uchar'
 FORMAT_FLOAT = '--format=float'
 FORMAT_WAV = '--format=wav'
