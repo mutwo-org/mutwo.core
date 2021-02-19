ef1ebf862d52ffdb712a49385d4372a8c1956f13
levinericzimmermann <levin.eric.zimmermann@posteo.eu>
2021-02-19 23:58:19 +0100

    slightly improved docstring

--- mutwo/converters/__init__.py
+++ mutwo/converters/__init__.py
@@ -1,9 +1,12 @@
 """Module that contains several subpackages to convert data from and to mutwo.
 
-The subpackages differentiate in the conversion mapping:
-    - backends:     external data -> mutwo objects
-    - frontends:    mutwo objects -> external data
-    - symmetrical:  mutwo objects -> mutwo objects
+The subpackages differentiate in their conversion mapping:
+    - backends:     external data                   -> mutwo or generic Python objects
+    - frontends:    mutwo or generic Python objects -> external data
+    - symmetrical:  mutwo or generic Python objects -> mutwo or generic Python objects
+
+*External data* can be a file, a Python object from a third-party library or a Stream
+object (that can be started to play for triggering events in real-time).
 """
 
 from . import abc
