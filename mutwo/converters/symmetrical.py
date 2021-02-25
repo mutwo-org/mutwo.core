603508d9eb1aaf569605b8352e203723912be4cc
levinericzimmermann <levin.eric.zimmermann@posteo.eu>
2021-02-25 12:41:41 +0100

    used average sound pressure for balancing out loudspeakers frequency response & improved docstring of LoudnessToAmplitudeConverter

--- mutwo/converters/symmetrical.py
+++ mutwo/converters/symmetrical.py
@@ -285,10 +285,16 @@ class TempoConverter(converters.abc.Converter):
 class LoudnessToAmplitudeConverter(converters.abc.Converter):
     """Make an approximation of the needed amplitude for a perceived Loudness.
 
+    :param perceived_loudness_in_sone: The subjectively perceived loudness that
+        the resulting signal shall have (in the unit `Sone`).
+    :param loudspeaker_frequency_response: Optionally the Frequency Response
+        of the used loudspeaker can be added for balancing out uneven curves in
+        the loudspeakers frequency response. The frequency response is defined
+        with an ``expenvelope.Envelope`` object.
+    :param interpolation_order: The interpolation order of the equal loudness
+        contour interpolation.
+
     The converter works best with pure sine waves.
-    The formula takes into account the frequency of the output signal and the
-    frequency response of the loudspeaker. The frequency response is defined
-    with an expenvelope.Envelope object.
     """
 
     # roughly the sound of a mosquito flying 3 m away
@@ -308,8 +314,8 @@ class LoudnessToAmplitudeConverter(converters.abc.Converter):
             perceived_loudness_in_phon, interpolation_order
         )
         self._loudspeaker_frequency_response = loudspeaker_frequency_response
-        self._loudspeaker_frequency_response_maxima = (
-            loudspeaker_frequency_response.max_level()
+        self._loudspeaker_frequency_response_average = (
+            loudspeaker_frequency_response.average_level()
         )
 
     @staticmethod
@@ -354,17 +360,18 @@ class LoudnessToAmplitudeConverter(converters.abc.Converter):
             self._equal_loudness_contour_interpolation(frequency)
         )
         # (2) figure out the produced soundpressure of the loudspeaker depending
-        #     on the frequency
+        #     on the frequency (for balancing uneven frequency responses of
+        #     loudspeakers)
         produced_soundpressure_for_1_watt_1_meter_depending_on_loudspeaker = self._loudspeaker_frequency_response.value_at(
             frequency
         )
-        difference_to_maxima = (
-            self._loudspeaker_frequency_response_maxima
+        difference_to_average = (
+            self._loudspeaker_frequency_response_average
             - produced_soundpressure_for_1_watt_1_meter_depending_on_loudspeaker
         )
         sound_pressure_level_for_pereived_loudness_based_on_speaker = (
             sound_pressure_level_for_perceived_loudness_based_on_frequency
-            + difference_to_maxima
+            + difference_to_average
         )
         amplitude_ratio = self.decibel_to_amplitude_ratio(
             sound_pressure_level_for_pereived_loudness_based_on_speaker,
