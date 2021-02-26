7efc8717d0b1d8bd44a36196b63cab16327c6a68
levinericzimmermann <levin.eric.zimmermann@posteo.eu>
2021-02-26 15:46:36 +0100

    added docstring

--- mutwo/converters/abc.py
+++ mutwo/converters/abc.py
@@ -1,3 +1,5 @@
+"""This module defines the public api for any converter class."""
+
 import abc
 import typing
 
@@ -5,7 +7,13 @@ __all__ = ("Converter",)
 
 
 class Converter(abc.ABC):
-    """Abstract base class for all Converter classes."""
+    """Abstract base class for all Converter classes.
+
+    Converter classes are defined as classes that convert data between
+    two different encodings. Their only public method (besides initialisation)
+    should be a `convert` method that has exactly one argument (the data that
+    should be converted).
+    """
 
     @abc.abstractmethod
     def convert(self, event_or_parameter_or_file_to_convert: typing.Any) -> typing.Any:
