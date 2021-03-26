28ab9b342f74a9fccb153c591ba2ee77c49e6046
levinericzimmermann <levin.eric.zimmermann@posteo.eu>
2021-03-26 12:31:38 +0100

    Add upper boundary for dependencies in setuptools
    Starting from this commit, we will follow the recommended version
    control standard. Therefore we will limit all dependencies until
    to their next major release (assuming that the API keeps stable
    until then).

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 import setuptools
 
 extras_require = {
-    "pyo": "pyo>=1.0.3",
-    "midi": "mido>=1.2.9",
-    "reaper": "rpp>=0.4",
+    "pyo": "pyo>=1.0.3, <2",
+    "midi": "mido>=1.2.9, <2",
+    "reaper": "rpp>=0.4, <0.5",
 }
 
 extras_require.update({"all": list(extras_require.values())})
@@ -24,12 +24,12 @@ setuptools.setup(
     setup_requires=[],
     tests_require=["nose"],
     install_requires=[
-        "expenvelope>=0.6.5",
-        "primesieve>=2.0.0",
-        "numpy",
-        "scipy",
-        "natsort>=5.3.3",
+        "expenvelope>=0.6.5, <1",
+        "primesieve>=2.0.0, <3",
+        "numpy>=1.18, <2",
+        "scipy>=1.41, <2",
+        "natsort>=5.3.3, <6",
     ],
     extras_require=extras_require,
-    python_requires=">=3.7",
+    python_requires=">=3.7, <4",
 )
