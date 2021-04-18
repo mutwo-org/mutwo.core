30ce5e6e0fb1f4034f26d92cbe7b03126810cc74
levinericzimmermann <levin.eric.zimmermann@posteo.eu>
2021-04-18 22:59:14 +0200

    Add __eq__ method

--- mutwo/parameters/tempos.py
+++ mutwo/parameters/tempos.py
@@ -11,7 +11,7 @@ class TempoPoint(parameters.abc.Parameter):
         self,
         tempo_in_beats_per_minute: float,
         reference: constants.Real = 1,
-        textual_indication: typing.Optional[str] = None
+        textual_indication: typing.Optional[str] = None,
     ):
         self.tempo_in_beats_per_minute = tempo_in_beats_per_minute
         self.reference = reference
@@ -20,6 +20,28 @@ class TempoPoint(parameters.abc.Parameter):
     def __repr__(self) -> str:
         return "{}(BPM: {}, reference: {})".format(
             type(self).__name__, self.tempo_in_beats_per_minute, self.reference
+        )
+
+    def __eq__(self, other: object) -> bool:
+        attributes_to_compare = (
+            "tempo_in_beats_per_minute",
+            "reference",
+            "textual_indication",
+        )
+        try:
+            comparision_tuple = tuple(
+                getattr(other, attribute) for attribute in attributes_to_compare
+            )
+        except AttributeError:
+            return False
+
+        return all(
+            tuple(
+                getattr(self, attribute) == value_of_other
+                for attribute, value_of_other in zip(
+                    attributes_to_compare, comparision_tuple
+                )
+            )
         )
 
     @property
