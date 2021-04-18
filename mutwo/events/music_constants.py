0811878f7bc04b3a38e8d05d215797971f9a44b0
levinericzimmermann <levin.eric.zimmermann@posteo.eu>
2021-04-18 14:33:49 +0200

    Add module docstring

--- mutwo/events/music_constants.py
+++ mutwo/events/music_constants.py
@@ -1,11 +1,13 @@
+"""Set default values for :class:`mutwo.events.music.NoteLike`."""
+
 from mutwo import parameters
 
 DEFAULT_PLAYING_INDICATORS_COLLECTION_CLASS = (
     parameters.playing_indicators.PlayingIndicatorCollection
 )
-"""default value for `playing_indicators` in :class:`mutwo.events.music.NoteLike`"""
+"""Default value for `playing_indicators` in :class:`mutwo.events.music.NoteLike`"""
 
 DEFAULT_NOTATION_INDICATORS_COLLECTION_CLASS = (
     parameters.notation_indicators.NotationIndicatorCollection
 )
-"""default value for `notation_indicators` in :class:`mutwo.events.music.NoteLike`"""
+"""Default value for `notation_indicators` in :class:`mutwo.events.music.NoteLike`"""
