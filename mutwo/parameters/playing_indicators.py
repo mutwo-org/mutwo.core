e8c995d76bb6f40a647b0f7d67cc0516d0df3c9f
levinericzimmermann <levin.eric.zimmermann@posteo.eu>
2021-04-18 14:02:53 +0200

    Add basic module documentation

--- mutwo/parameters/playing_indicators.py
+++ mutwo/parameters/playing_indicators.py
@@ -1,4 +1,48 @@
-"""
+"""Define playing indicators for simple events.
+
+This submodules provides several classes to add specific musical
+playing techniques to :class:`mutwo.events.basic.SimpleEvent` objects.
+They mostly derive from traditional Western playing techniques and their
+notation. Unlike indicators of the :mod:`mutwo.parameters.notation_indicators`
+module, playing indicators have an effect of the played music and aren't
+merely specifications of representation. The proper way to handle
+playing  indicators should be via a :class:`PlayingIndicatorCollection`
+object that should be attached to the respective :class:`SimpleEvent`.
+The collection contains all possible playing indicators which are defined
+in this module. :class:`mutwo.events.music.NoteLike` contain by default
+a playing indicator collection.
+
+There are basically two different types of playing indicators:
+
+1, Playing indicators which can only be on or off (for instance
+    ``bartok_pizzicato``, ``prall`` or ``laissez_vibrer``). They
+    have a :attr:`is_active` attribute which can either be :obj:`True`
+    or :obj:`False`.
+2. Playing indicators with one or more arguments (for instance
+    :class:`Tremolo` with :attr:`n_flags` or :class:`Arpeggio` with
+    :attr:`direction`). Their :attr:`is_active` attribute can't be
+    set by the user and get automatically initialised depending on
+    if all necessary attributes are defined (then active) or
+    if any of the necessary attributes is set to :obj:`None` (then
+    not active).
+
+**Example:**
+
+Set playing indicators of :class:`NoteLike`:
+
+>>> from mutwo.events import music
+>>> my_note = music.NoteLike('c', 1 / 4, 'mf')
+>>> my_note.playing_indicators.articulation.name = "."  # add staccato
+>>> my_chord = music.NoteLike('c e g', 1 / 2, 'f')
+>>> my_chord.playing_indicators.arpeggio.direction= "up"  # add arpeggio
+>>> my_chord.playing_indicators.laissez_vibrer.is_active = True  # and laissez_vibrer
+
+Attach :class:`PlayingIndicatorCollection` to :class:`SimpleEvent`:
+
+>>> from mutwo.events import basic
+>>> from mutwo.parameters import playing_indicators
+>>> my_simple_event = basic.SimpleEvent()
+>>> my_simple_event.playing_indicators = playing_indicators.PlayingIndicatorCollection()
 """
 
 import dataclasses
