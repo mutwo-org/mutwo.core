b5cda9cb3b265dc266d8c443976f9fbd77d554a5
levinericzimmermann <levin.eric.zimmermann@posteo.eu>
2021-04-24 01:15:41 +0200

    Improve for first pypi release
    This commit improved setup.py and added pyproject.toml for the
    the first experimental pypi release.

--- setup.py
+++ setup.py
@@ -1,5 +1,9 @@
 import setuptools  # type: ignore
 
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
 extras_require = {
     "pyo": ["pyo>=1.0.3, <2"],
     "midi": ["mido>=1.2.9, <2"],
@@ -14,14 +18,15 @@ extras_require.update(
 
 setuptools.setup(
     name="mutwo",
-    version="0.0.01",
+    version="0.1.0",
     license="GPL",
-    description="representation of timebased events",
-    author=(
-        "Tim Pauli <tim.pauli@folkwang-uni.de>, Levin Zimmermann"
-        " <levin-eric.zimmermann@folkwang-uni.de>"
-    ),
+    description="event based framework for generative art",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="Tim Pauli,  Levin Eric Zimmermann",
+    author_email="tim.pauli@folkwang-uni.de, levin.eric.zimmermann@posteo.eu",
     url="https://github.com/mutwo-org/mutwo",
+    project_urls={"Documentation": "https://mutwo.readthedocs.io/en/latest/"},
     packages=[
         package for package in setuptools.find_packages() if package[:5] != "tests"
     ],
