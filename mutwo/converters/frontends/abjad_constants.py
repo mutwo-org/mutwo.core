76550877f4b8d4e640fb0effd7f13a89601d468c
levinericzimmermann <levin.eric.zimmermann@posteo.eu>
2021-05-11 19:47:00 +0200

    Simplify process of adding new indicators by users
    This commit simplifies the process of adding new playing and notation
    indicators by users. Before this commit the mapping of playing
    indicators to abjad attachments was hard coded in abjad_constants.
    Now this mapping gets dynamically generated when initializing a new
    SequentialEventToAbjadVoiceConverter.

--- mutwo/converters/frontends/abjad_constants.py
+++ mutwo/converters/frontends/abjad_constants.py
@@ -4,36 +4,13 @@
 import inspect
 
 from mutwo.converters.frontends import abjad_attachments
-from mutwo.parameters import notation_indicators
-from mutwo.parameters import playing_indicators
 from mutwo.utilities import tools
 
 
-_abjad_attachment_class_name_to_class = {
+DEFAULT_ABJAD_ATTACHMENT_NAME_TO_ABJAD_ATTACHMENT = {
     tools.class_name_to_object_name(cls_name): cls
     for cls_name, cls in inspect.getmembers(abjad_attachments, inspect.isclass)
     if not inspect.isabstract(cls)
 }
-
-PLAYING_INDICATOR_TO_ABJAD_ATTACHMENT = {
-    playing_indicator_name: _abjad_attachment_class_name_to_class[
-        playing_indicator_name
-    ]
-    for playing_indicator_name in playing_indicators.PlayingIndicatorCollection.__dataclass_fields__.keys()  # type: ignore
-    if playing_indicator_name in _abjad_attachment_class_name_to_class
-}
-"""Mapping of playingindicator to Abjad attachment."""
-
-NOTATION_INDICATOR_TO_ABJAD_ATTACHMENT = {
-    notation_indicator_name: _abjad_attachment_class_name_to_class[
-        notation_indicator_name
-    ]
-    for notation_indicator_name in notation_indicators.NotationIndicatorCollection.__dataclass_fields__.keys()  # type: ignore
-    if notation_indicator_name in _abjad_attachment_class_name_to_class
-}
-"""Mapping of notation indicator to Abjad attachment."""
-
-AVAILABLE_ABJAD_ATTACHMENTS = _abjad_attachment_class_name_to_class.keys()
-"""All available Abjad attachments."""
-
-del _abjad_attachment_class_name_to_class
+"""Default value for argument `abjad_attachment_name_to_abjad_attachment` in
+:class:`~mutwo.converters.frontends.SequentialEventToAbjadVoiceConverter`."""
--- mutwo/events/music_constants.py
+++ mutwo/events/music_constants.py
@@ -5,9 +5,15 @@ from mutwo import parameters
 DEFAULT_PLAYING_INDICATORS_COLLECTION_CLASS = (
     parameters.playing_indicators.PlayingIndicatorCollection
 )
-"""Default value for `playing_indicators` in :class:`mutwo.events.music.NoteLike`"""
+"""Default value for :attr:`~mutwo.events.music.NoteLike.playing_indicators`
+in :class:`~mutwo.events.music.NoteLike` and for
+`referential_playing_indicator_collection` in
+:class:`~mutwo.converters.frontends.abjad.SequentialEventToAbjadVoiceConverter`"""
 
 DEFAULT_NOTATION_INDICATORS_COLLECTION_CLASS = (
     parameters.notation_indicators.NotationIndicatorCollection
 )
-"""Default value for `notation_indicators` in :class:`mutwo.events.music.NoteLike`"""
+"""Default value for :attr:`~mutwo.events.music.NoteLike.notation_indicators`
+in :class:`~mutwo.events.music.NoteLike` and for
+`referential_notation_indicator_collection` in
+:class:`~mutwo.converters.frontends.abjad.SequentialEventToAbjadVoiceConverter`"""
