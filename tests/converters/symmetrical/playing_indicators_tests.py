71e58cf3762b265c2d272c7c0924f20416ff62d9
levinericzimmermann <levin.eric.zimmermann@posteo.eu>
2021-05-13 20:03:22 +0200

    Add test for PlayingIndicatorsConverter

--- tests/converters/symmetrical/playing_indicators_tests.py
+++ tests/converters/symmetrical/playing_indicators_tests.py
@@ -25,5 +25,28 @@ class ArpeggioConverterTest(unittest.TestCase):
         self.assertEqual(converter.convert(note_like), arpeggio)
 
 
+class PlayingIndicatorsConverterTest(unittest.TestCase):
+    def test_convert(self):
+        converter = symmetrical.playing_indicators.PlayingIndicatorsConverter(
+            [
+                symmetrical.playing_indicators.ArpeggioConverter(
+                    duration_for_each_attack=0.1
+                )
+            ]
+        )
+        note_like = music.NoteLike("c g e b", duration=5)
+        note_like.playing_indicators.arpeggio.direction = "up"
+        arpeggio = basic.SequentialEvent(
+            [
+                music.NoteLike(pitch, duration)
+                for pitch, duration in zip("c e g b".split(" "), (0.1, 0.1, 0.1, 4.7))
+            ]
+        )
+        for note in arpeggio:
+            note.playing_indicators.arpeggio.direction = "up"
+
+        self.assertEqual(converter.convert(note_like), arpeggio)
+
+
 if __name__ == "__main__":
     unittest.main()
