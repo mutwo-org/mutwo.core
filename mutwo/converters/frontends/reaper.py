8046b5cb0a41cf8e099703f0fe4d653a549c0f63
levinericzimmermann <levin.eric.zimmermann@posteo.eu>
2021-10-23 21:06:52 +0200

    Removed deprecated ReaperFileConverter and reaper_constants

--- mutwo/converters/frontends/reaper.py
+++ mutwo/converters/frontends/reaper.py
@@ -1,88 +1,11 @@
 import typing
 
-import rpp  # type: ignore
-
-from mutwo.converters.frontends import reaper_constants
 
 from mutwo import converters
 from mutwo import events
 from mutwo import parameters
-from mutwo.utilities import tools
 
-__all__ = ("ReaperFileConverter", "ReaperMarkerConverter")
-
-
-class ReaperFileConverter(converters.abc.Converter):
-    def __init__(self):
-        self.reaper_project = self.create_reaper_project()
-
-    @staticmethod
-    def create_reaper_project() -> rpp.Element:
-        return rpp.Element(
-            tag="REAPER_PROJECT",
-            attrib=["0.1", "6.16/linux-x86_64", "1607708341"],
-            children=[],
-        )
-
-    def add_track(self, name: str = "") -> rpp.Element:
-        track = rpp.Element(
-            tag="TRACK",
-            children=[["NAME", name], rpp.Element(tag="FXCHAIN", children=[],),],
-        )
-        self.reaper_project.append(track)
-        return track
-
-    def get_track(self, name: str) -> rpp.Element:
-        for track in self.reaper_project.findall(".//TRACK"):
-            current_trackname = track.find(".//NAME")
-            if current_trackname[1] == name:
-                return track
-
-    @staticmethod
-    def add_fx(
-        track: rpp.Element, fx_info: reaper_constants.VstInfo, name: str = ""
-    ) -> rpp.Element:
-        fx = rpp.Element(
-            tag="VST", attrib=[name, fx_info.filename], children=fx_info.binary
-        )
-        fxchain = track.find(".//FXCHAIN")
-        fxchain.append(fx)
-        return fx
-
-    @staticmethod
-    def get_all_fx(track) -> typing.List[rpp.Element]:
-        fxchain = track.find(".//FXCHAIN")
-        return fxchain.findall(".//VST")
-
-    @staticmethod
-    def add_parameter_envelope(
-        track: rpp.Element,
-        fx: rpp.Element,
-        parameter_name: str,
-        envelope_events: events.basic.SequentialEvent,
-    ) -> rpp.Element:
-        """events need a value attribute"""
-        envelope = rpp.Element(
-            tag="PARMENV",
-            attrib=[parameter_name, "0", "1", "0.5"],
-            children=[
-                ["ACT", "1", "-1"],
-                ["VIS", "1", "1", "1"],
-                ["DEFSHAPE", "0", "-1", "-1"],
-            ],
-        )
-        envelope_points = [
-            ["PT", time, event.value, 0]
-            for time, event in zip(envelope_events.absolute_times, envelope_events)
-        ]
-        envelope.extend(envelope_points)
-        fxchain = track.find(".//FXCHAIN")
-        tools.insert_next_to(fxchain, fx, 1, envelope)
-        return envelope
-
-    def convert(self, event: events.abc.Event):
-        with open(self.path, "w") as file:
-            rpp.dump(self.reaper_project, file)
+__all__ = ("ReaperMarkerConverter",)
 
 
 class ReaperMarkerConverter(converters.abc.EventConverter):
--- tests/converters/frontends/reaper_tests.py
+++ tests/converters/frontends/reaper_tests.py
@@ -3,10 +3,5 @@ import unittest
 from mutwo.converters.frontends import reaper
 
 
-class TempoConverterTest(unittest.TestCase):
-    def setUp(self):
-        self.converter = reaper.ReaperFileConverter()
-
-
 if __name__ == "__main__":
     unittest.main()
