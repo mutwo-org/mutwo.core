6a3053f684211d356a0cdf9f94c7de55a6e9b9be
Levin Eric Zimmermann <levin.eric.zimmermann@posteo.eu>
2021-11-26 18:17:33 +0000

    Remove 'pyo' from setup
    For now it doesn't seem that a pyo frontend will be added soon,
    therefore it should be removed from the setup file.

--- setup.py
+++ setup.py
@@ -5,7 +5,6 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 extras_require = {
-    "pyo": ["pyo>=1.0.3, <2"],
     "midi": ["mido>=1.2.9, <2"],
     "reaper": ["rpp>=0.4, <0.5"],
     "abjad": [
