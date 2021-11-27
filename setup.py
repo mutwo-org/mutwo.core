a72eea2099bb91fa42984172ee20261acb4fd26f
Levin Eric Zimmermann <levin.eric.zimmermann@posteo.eu>
2021-11-27 11:40:21 +0000

    Bump to version 0.38.1

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ extras_require.update(
 
 setuptools.setup(
     name="mutwo",
-    version="0.38.0",
+    version="0.38.1",
     license="GPL",
     description="event based framework for generative art",
     long_description=long_description,
