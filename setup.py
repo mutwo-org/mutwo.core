ded6049604fd06fd55a80cd7e81f3a1d30d9baac
Levin Eric Zimmermann <levin.eric.zimmermann@posteo.eu>
2022-01-11 20:21:40 +0000

    Add lost tempos test

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ extras_require = {"testing": ["nose", "pillow>=8.2.0, <9.0.0"]}
 
 setuptools.setup(
     name="mutwo",
-    version="0.49.0",
+    version="0.49.1",
     license="GPL",
     description="event based framework for generative art",
     long_description=long_description,
