61ad8d406ea4f77c61f49350eb7697c1e1ef02e4
Levin Eric Zimmermann <levin.eric.zimmermann@posteo.eu>
2022-01-14 20:19:46 +0000

    Bump to bf version 4

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 import setuptools  # type: ignore
 
-MAJOR, MINOR, PATCH = 0, 49, 3
+MAJOR, MINOR, PATCH = 0, 49, 4
 VERSION = f"{MAJOR}.{MINOR}.{PATCH}"
 """This project uses semantic versioning.
 See https://semver.org/
