337cfa0c42cfa3a0974731008385a3264942e136
Levin Eric Zimmermann <levin.eric.zimmermann@posteo.eu>
2022-10-06 07:52:40 +0200

    Update python-ranges

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ setuptools.setup(
     install_requires=[
         "numpy>=1.18, <2.00",
         "scipy>=1.4.1, <2.0.0",
-        "python-ranges>=0.2.0, <1.0.0",
+        "python-ranges>=1.2.0, <2.0.0",
     ],
     extras_require=extras_require,
     python_requires=">=3.9, <4",
