97aea97f996973955889630c437ceaea405ea0a7
Levin Eric Zimmermann <levin.eric.zimmermann@posteo.eu>
2022-10-06 16:50:34 +0200

    Update nix to v0.67.0

--- default.nix
+++ default.nix
@@ -11,7 +11,9 @@ let
       rev = "38ac789b61e1e33d1a8be999ca969f909bb652c0";
       sha256 = "sha256-oRQCtDBQnViNP6sJZU0NqFWkn2YpcIeGWmfx3Ne/n2c=";
     };
+    # TypeError: calling <class 'ranges.RangeDict.RangeDict'> returned {}, not a test
     doCheck = false;
+    propagatedBuildInputs = [ python39Packages.pytest ];
   };
 
 in
@@ -21,8 +23,8 @@ in
     src = fetchFromGitHub {
       owner = "mutwo-org";
       repo = name;
-      rev = "337cfa0c42cfa3a0974731008385a3264942e136";
-      sha256 = "sha256-2fc7IMLyHwIQMOsG5W4oRbAN4+l4LFP7mhkNP5+VO+M=";
+      rev = "9accb5f03d0155a4872cb429111b7d6c5a47ce28";
+      sha256 = "sha256-cr30PxPtePZCwDYsfxM7l+M5fDNrxqh8OybLiHz+Ip4=";
     };
     propagatedBuildInputs = [ 
       python39Packages.numpy
