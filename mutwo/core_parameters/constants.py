d5f038f984cb51ab3f2beb3123dfdf6585eb409f
levinericzimmermann <levinericzimmermann@posteo.eu>
2022-11-05 21:38:46 +0100

    Add docstrings

--- mutwo/core_parameters/constants.py
+++ mutwo/core_parameters/constants.py
@@ -3,9 +3,18 @@ import typing
 import ranges
 
 TempoInBeatsPerMinute = float
+"""Type alias for `TempoInBeatsPerMinute`. Used in
+`core_parameters.abc.TempoPoint`"""
+
 TempoRangeInBeatsPerMinute = ranges.Range
+"""Type alias for `TempoRangeInBeatsPerMinute`. Used in
+`core_parameters.abc.TempoPoint`"""
+
 TempoOrTempoRangeInBeatsPerMinute = typing.Union[
     TempoInBeatsPerMinute, TempoRangeInBeatsPerMinute
 ]
+"""Type alias for `TempoOrTempoRangeInBeatsPerMinute`. Used in
+`core_parameters.abc.TempoPoint`"""
 
+# Cleanup
 del ranges, typing
