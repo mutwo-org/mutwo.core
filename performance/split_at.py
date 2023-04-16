1273b8b7dedd04b3705aab8da7cb8408c55b8a08
Levin Eric Zimmermann <levin.eric.zimmermann@posteo.eu>
2023-04-17 00:03:45 +0200

    performance += split_at test
    Before 9c2a4ff279ddc4c5cb408d4b61bd671f32174e72 'split_at' was extremely
    slow when called with many split positions. We therefore need a
    performance test to benchmark how fast it currently is and to avoid
    regressions.

---
+++ performance/split_at.py
@@ -0,0 +1,21 @@
+import cProfile
+import random
+
+from mutwo import core_events
+
+random.seed(100)
+
+
+def t():
+    s = core_events.SequentialEvent(
+        [core_events.SimpleEvent(random.uniform(1, 3)) for _ in range(200)]
+    )
+    duration = s.duration.duration_in_floats
+    split_time_list = sorted([random.uniform(0, duration) for _ in range(70)])
+    s.split_at(*split_time_list)
+    # for t in split_time_list:
+    #     s.split_at(t)
+
+
+# 6.4
+cProfile.run(t.__code__, sort=2)
