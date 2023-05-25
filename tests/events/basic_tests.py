1a7c0da2b3f70c10d8d059633c21e888aa12b70c
Levin Eric Zimmermann <levin.eric.zimmermann@posteo.eu>
2023-05-25 12:29:27 +0200

    qa: Add test for setting duration of empty complex event

--- tests/events/basic_tests.py
+++ tests/events/basic_tests.py
@@ -385,6 +385,15 @@ class SequentialEventTest(unittest.TestCase, EventTest):
         self.assertEqual(sequential_event[0].duration, 2.5)
         self.assertEqual(sequential_event[1].duration, 2.5)
 
+    def test_set_duration_of_empty_event(self):
+        sequential_event = core_events.SequentialEvent([])
+        self.assertRaises(
+            core_utilities.CannotSetDurationOfEmptyComplexEvent,
+            sequential_event.set,
+            "duration",
+            1,
+        )
+
     def test_get_absolute_time_tuple(self):
         result = tuple(self.sequence.absolute_time_tuple)
         self.assertEqual(
@@ -1190,6 +1199,7 @@ class SimultaneousEventTest(unittest.TestCase, EventTest):
                 se([s(2)]),
             ]
         )
+
         self.assertEqual(
             si_test.concatenate_by_index(self.nested_sequence).metrize(), si_ok
         )
