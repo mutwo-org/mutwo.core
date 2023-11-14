4962bf886272c6f612bdea9a6d045809ae574040
Levin Eric Zimmermann <levin.eric.zimmermann@posteo.eu>
2023-11-14 11:08:21 +0100

    utilities/MutwoObject.copy: ↑ performance x2
    This patch improves the performance of 'MutwoObject.copy' operation by
    200% (tested on calls of events). This performance improvement is very
    important, as operations of mutwo events often use 'copy', and due to
    the centrality of events the whole system becomes faster.

--- mutwo/core_utilities/mutwo.py
+++ mutwo/core_utilities/mutwo.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 
 import copy
+import pickle
 import typing
 
 __all__ = ("MutwoObject",)
@@ -16,4 +17,13 @@ class MutwoObject(object):
 
     def copy(self: T) -> T:
         """Return a deep copy of mutwo object."""
-        return copy.deepcopy(self)
+        # NOTE: using pickle speeds up the copy operation by ~200%.
+        # Because we often need to copy events in 'mutwo', this is a very
+        # useful speedup for the whole ecosystem.
+        try:
+            return pickle.loads(pickle.dumps(self))
+        # Some objects as lambda functions or modules can't be pickled: in
+        # case our MutwoObject contains such unpickable objects, fall
+        # back to less efficient deepcopy method.
+        except (pickle.PicklingError, TypeError):
+            return copy.deepcopy(self)
