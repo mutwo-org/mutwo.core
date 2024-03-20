2c73955e0e4f9e86a36b355e7262ba593ee1cbc6
Levin Eric Zimmermann <levin.eric.zimmermann@posteo.eu>
2024-03-20 19:33:36 +0100

    events: Drop 'RelativeEnvelope'
    This event has been added in https://github.com/mutwo-org/mutwo.core/commit/9f601a85ceb2adfe42d23bb5a8371981f9f113d1 in
    order to support glissandi (introduced few commits later in https://github.com/mutwo-org/mutwo.core/commit/df609dcf1900b3543908239b56af88e38d965f23).
    With recent patch https://github.com/mutwo-org/mutwo.core/commit/05a919c93d8ba9ce69a8a3328d45d4144c7ea296 that aims to
    unify the way how continuous/dynamic parameters are treated, we would
    also no longer need 'RelativeEnvelope' for glissandi (once 'mutwo.music'
    has been adjusted to 'mutwo.core >= 2.0.0'), because glissandi would be
    described by absolute points. Now, even if we later on re-introduce
    generic relative parameters, we may not necessarily need a
    'RelativeEnvelope' class as this only provides the small '.resolve'
    method that perhaps could also be re-written in a better way.

--- tests/events/envelopes_tests.py
+++ tests/events/envelopes_tests.py
@@ -352,23 +352,5 @@ class EnvelopeTest(unittest.TestCase, ComplexEventTest):
         )
 
 
-class RelativeEnvelopeTest(unittest.TestCase):
-    def setUp(cls):
-        cls.envelope = core_events.RelativeEnvelope(
-            [
-                [0, 0],
-                [5, 5],
-                [10, 10],
-            ],
-            base_parameter_and_relative_parameter_to_absolute_parameter=lambda base_parameter, relative_parameter: base_parameter
-            + relative_parameter,
-        )
-
-    def test_resolve(self):
-        resolved_envelope = self.envelope.resolve(duration=1, base_parameter=100)
-        self.assertEqual(resolved_envelope.duration, core_parameters.DirectDuration(1))
-        self.assertEqual(resolved_envelope.value_tuple, (100, 105, 110))
-
-
 if __name__ == "__main__":
     unittest.main()
